331641d76257d2955e9152e843287b1277bb30d9
Chris <jochchristopher@gmail.com>
2024-01-29 14:50:57 +0100

    changed run script to check for image

--- run.bat
+++ run.bat
@@ -1,4 +1,15 @@
 @echo off
+echo Checking if the Docker image is already built...
+docker image inspect react-mines >nul 2>&1
+
+if %errorlevel% neq 0 (
+    echo Image not found. Building Docker Image...
+    docker build -t react-mines .
+    echo Build Complete!
+) else (
+    echo Image found. Skipping build.
+)
+
 echo Running Docker Container...
-docker run -p 80:80 --name react-mines react-mines
+docker run -p 80:80 -d --name react-mines react-mines
 echo Container is running on port 80
